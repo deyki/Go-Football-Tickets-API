20463524ba4a7e2ddda1fa94322cfda622384b0c
Nikolay Nikolaev <111266016+deyki@users.noreply.github.com>
2022-12-12 13:10:43 +0200

    Update errors.go

--- util/errors.go
+++ util/errors.go
@@ -10,7 +10,7 @@ type ErrorMessage struct {
 
 func (e ErrorMessage) ErrorLoadingEnvFile() *ErrorMessage {
 	return &ErrorMessage{
-		HttpStatus: http.StatusInternalServerError,
+		HttpStatus: 	http.StatusInternalServerError,
 		Message:	"Error loading .env file!",
 	}
 }
@@ -18,7 +18,7 @@ func (e ErrorMessage) ErrorLoadingEnvFile() *ErrorMessage {
 
 func (e ErrorMessage) FailedToOpenDB() *ErrorMessage {
 	return &ErrorMessage{
-		HttpStatus: http.StatusInternalServerError,
+		HttpStatus: 	http.StatusInternalServerError,
 		Message: 	"Failed to open db!",
 	}
 }
@@ -26,7 +26,7 @@ func (e ErrorMessage) FailedToOpenDB() *ErrorMessage {
 
 func (e ErrorMessage) FailedToCreateHashFromPassword() *ErrorMessage {
 	return &ErrorMessage{
-		HttpStatus: http.StatusInternalServerError,
+		HttpStatus: 	http.StatusInternalServerError,
 		Message:	"Failed to create hash from password!",
 	}
 }
@@ -34,7 +34,7 @@ func (e ErrorMessage) FailedToCreateHashFromPassword() *ErrorMessage {
 
 func (e ErrorMessage) AdminNotFound() *ErrorMessage {
 	return &ErrorMessage{
-		HttpStatus: http.StatusNotFound,
+		HttpStatus: 	http.StatusNotFound,
 		Message: 	"Admin not found!",
 	}
 }
@@ -42,7 +42,7 @@ func (e ErrorMessage) AdminNotFound() *ErrorMessage {
 
 func (e ErrorMessage) FailedToCreateJWToken() *ErrorMessage {
 	return &ErrorMessage{
-		HttpStatus: http.StatusInternalServerError,
+		HttpStatus: 	http.StatusInternalServerError,
 		Message:	"Failed to create JWToken!",
 	}
 }
@@ -50,7 +50,7 @@ func (e ErrorMessage) FailedToCreateJWToken() *ErrorMessage {
 
 func (e ErrorMessage) TicketNotFound() *ErrorMessage {
 	return &ErrorMessage{
-		HttpStatus: http.StatusNotFound,
+		HttpStatus: 	http.StatusNotFound,
 		Message: 	"Ticket not found!",
 	}
 }
